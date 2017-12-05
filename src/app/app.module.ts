18300abd40ca0b3ea2c0cf5fd1505d4642924fb4
Ciara <32096395+ciaralynch@users.noreply.github.com>
2017-12-05 13:30:06 +0000

    Added components and installed Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditprofileComponent } from './editprofile/editprofile.component';
+import { SearchComponent } from './search/search.component';
+import { ReviewersComponent } from './reviewers/reviewers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent,
+    ListComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    EditprofileComponent,
+    SearchComponent,
+    ReviewersComponent
   ],
   imports: [
     BrowserModule
