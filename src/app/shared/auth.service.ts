f9dfd5f600da7fe58c503ebc3bbb397456d1063c
Ciara <32096395+ciaralynch@users.noreply.github.com>
2017-12-05 13:37:21 +0000

    Added auth and notification services.

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/observable';
+import { NotificationService } from '../shared/notification.service';
+
+@Injectable()
+export class AuthService {
+  user: Observable<firebase.User>;
+
+  constructor(private firebaseAuth: AngularFireAuth) {
+    this.user = firebaseAuth.authState;
+  }
+
+  signup(email: string, password: string) {
+    this.firebaseAuth
+      .auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Success!', value);
+      })
+      .catch(err => {
+        console.log('Something went wrong:', err.message);
+      });
+  }
+
+  login(email: string, password: string) {
+    this.firebaseAuth
+      .auth
+      .signInWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Nice, it worked!');
+      })
+      .catch(err => {
+        console.log('Something went wrong:', err.message);
+      });
+  }
+
+  logout() {
+    this.firebaseAuth
+      .auth
+      .signOut();
+  }
+}
