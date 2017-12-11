508f0cc3c44ecbccd210d3f6bb089e18cc1a414c
S00156269 <s00156269@mail.itsligo.ie>
2017-12-11 10:37:30 +0000

    progress

--- src/app/reviewer.ts
+++ src/app/reviewer.ts
@@ -1,14 +1,7 @@
 export class Reviewer {
-    UID: string;
     Name: string;
-    Pic: string;
     Bio: string;
-    FaveGenres: boolean[]
-    FaveMovies: {
-        Movie1: string,
-        Movie2: string,
-        Movie3: string
-    }
-    Reviews: string[]
-    Lists: string[]
+    FaveGenres: string[];
+    FaveMovies: string[];
+    Reviews: string[];
 }
