7500003b09a15553d4f00537e52d26c20d2293ff
S00156269 <s00156269@mail.itsligo.ie>
2017-12-14 19:41:43 +0000

    Fixed reviews and most functionality added

--- src/app/editprofile/editprofile.component.ts
+++ src/app/editprofile/editprofile.component.ts
@@ -10,6 +10,7 @@ import { Reviewer } from '../reviewer';
 import { imdbService } from '../shared/imdb.service';
 import { IShows } from '../shared/iShows';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Review } from '../review';
 
 @Component({
   selector: 'app-editprofile',
@@ -18,36 +19,92 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 
 export class EditprofileComponent implements OnInit {
+  /*
+   * Tried displaying the data on the page, but it didn't like that at all.
+   * It does update the data on the backend.
+   */
   form: FormGroup;
   filteredMovies: string[];
   currentReviewer: Reviewer;
   success: boolean;
-  faveGenres: string[] = [];
-  faveMovies: string[] = [];
   availableGenres: string[];
   shows: IShows[];
   filteredShows: IShows[];
   errorMessage: any;
-  
+  uid: string;
   constructor(public authService: AuthService, public afa: AngularFireAuth, private _iMDBService: imdbService, private db: AngularFireDatabase, private data: DataServiceService, private router: Router) {
     this.availableGenres = ["Horror", "Action", "Sci-Fi", "Thriller", "Comedy"];
     this.success = true;
     this.currentReviewer = new Reviewer;
+    this.currentReviewer.FaveMovies = [];
+    this.currentReviewer.FaveGenres = [];
+    /* Had to repeat this code here because the constructor in the service
+ * doesn't fire fast enough, so it wouldn't return the user.
+ */
+    this.afa.authState.subscribe((resp) => {
+      if (resp != null) {
+        if (resp.uid) {
+          this.uid = resp.uid;
+          this.populateFields();
+        }
+      }
+      else {
+        this.router.navigate(['login']);
+      }
+    });
+  }
+  populateFields() {
+    //in case the user wants to edit already existing stuff
+    this.data.getUser(this.uid).subscribe(value => {
+      this.currentReviewer = value;
+      if (this.currentReviewer.FaveMovies == undefined)
+        this.currentReviewer.FaveMovies = [];
+      if (this.currentReviewer.FaveMovies == undefined)
+        this.currentReviewer.FaveMovies = [];
+      console.log(this.currentReviewer);
+    });
+  }
+  getName() {
+    //gets details
+    if (this.currentReviewer) {
+      return this.currentReviewer.Name;
+    }
+    else {
+      return "Name";
+    }
   }
 
-  createUserProfile(name, bio) {
-    if (name != null && name != "" && bio != null && bio != "") {
-      this.currentReviewer.Name = name;
-      this.currentReviewer.Bio = bio;
-      this.currentReviewer.FaveGenres = this.faveGenres;
-      this.currentReviewer.FaveMovies = this.faveMovies;
-      console.log(this.currentReviewer);
-      this.data.createUser(this.formatPost(this.currentReviewer));
-      this.router.navigate(['profile']);
-      }
-    else {
-      this.success = false;
+  getBio() {
+    if (this.currentReviewer) {
+      return this.currentReviewer.Bio;
     }
+    else {
+      return "Name";
+    }
+  }
+
+  checkGenres(value) {
+    if (this.currentReviewer) {
+      if (this.currentReviewer.FaveGenres.includes(value)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  createUserProfile(name, bio) {
+    this.currentReviewer.Name = name;
+    this.currentReviewer.Bio = bio;
+    this.currentReviewer.FaveMovies = this.currentReviewer.FaveMovies;
+    console.log(this.currentReviewer);
+    this.data.createUser(this.formatPost(this.currentReviewer));
+    //this.router.navigate(['profile']);
   }
 
   formatPost(user: Reviewer): any {
@@ -56,31 +113,36 @@ export class EditprofileComponent implements OnInit {
       "Bio": user.Bio,
       "FaveGenres": user.FaveGenres,
       "FaveMovies": user.FaveMovies,
-      "Reviews": user.Reviews
+      "reviews": user.reviews
     }
   }
 
 
   addFaveFilm(title) {
-    if (!this.faveMovies.includes(title)) {
-      this.faveMovies.push(title);
-      console.log(this.faveMovies);
+    if (!this.currentReviewer.FaveMovies.includes(title)) {
+      this.currentReviewer.FaveMovies.push(title);
+      console.log(this.currentReviewer.FaveMovies);
     }
   }
 
   removeFaveMovie(title: any) {
-    this.faveMovies.splice(this.faveMovies.indexOf(title), 1);
-    console.log(this.faveMovies);
+    this.currentReviewer.FaveMovies.splice(this.currentReviewer.FaveMovies.indexOf(title), 1);
+  }
+
+  deleteReview(review){
+    this.currentReviewer.reviews.splice(this.currentReviewer.reviews.indexOf(review), 1)
+    
+    console.log(this.currentReviewer);
   }
 
   getGenres(genre: string) {
-    if (!this.faveGenres.includes(genre)) {
-      this.faveGenres.push(genre);
-      console.log(this.faveGenres);
+    if (!this.currentReviewer.FaveGenres.includes(genre)) {
+      this.currentReviewer.FaveGenres.push(genre);
+      console.log(this.currentReviewer.FaveGenres);
     }
     else {
-      this.faveGenres.splice(this.faveGenres.indexOf(genre), 1);
-      console.log(this.faveGenres);
+      this.currentReviewer.FaveGenres.splice(this.currentReviewer.FaveGenres.indexOf(genre), 1);
+      console.log(this.currentReviewer.FaveGenres);
     }
   }
 
